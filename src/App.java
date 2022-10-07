84346a5f7829c11bdb51125488782e33a6a9f4fd
Matheus Valentim <114266242+NegaoValente@users.noreply.github.com>
2022-10-07 14:37:33 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Vamos lá né");
+        Scanner value= new Scanner(System.in);
+       
+       int i;
+          
+        int  x[] = new int[5];
+        int  y[] = new int[5];
+         int z[] = new int[10];
+ 
+         for(i=0;i<5; i++){
+            System.out.print("Digite o valor para x:");
+           x[i]=value.nextInt();
+         }   
+         for (i=0;i<5;i++){
+            System.out.print("Digite o valor para y:");
+            y[i]=value.nextInt();
+         }
+         
+        
+
+
+         }
+         
+        }
+    
